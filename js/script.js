f4d3272933994f2f12821176b9aa25229ead3064
Marcin <36379394+MarcinBB@users.noreply.github.com>
2018-03-26 19:38:44 +0200

    Update script.js
    small mistakes correct

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-function Phone (brand, price, color,) {
+function Phone (brand, price, color) {
   this.brand = brand
   this.price = price
   this.color = color
@@ -8,9 +8,9 @@ Phone.prototype.printInfo = function () {
   console.log('The phone brand is ' + this.brand + ', color is ' + this.color + ' and the price is ' + this.price + '.')
 }
 
-var iPhoneX = new Phone('Apple', 5729, 'space grey', 'cool')
+var iPhoneX = new Phone('Apple', 5729, 'space grey')
 iPhoneX.printInfo()
-var samsungGalaxyS9 = new Phone('Samsung', 3599, 'midnight black', 'cool enough')
+var samsungGalaxyS9 = new Phone('Samsung', 3599, 'midnight black')
 samsungGalaxyS9.printInfo()
-var onePlusOne5T = new Phone('OnePlus5T', 2799, 'lava red', "well... it's ok" + '.')
+var onePlusOne5T = new Phone('OnePlus5T', 2799, 'lava red')
 onePlusOne5T.printInfo()
